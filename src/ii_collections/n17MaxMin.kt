b9b1cf739e4d3340435c76fbce5cc3b98d630de7
Jerome Fosse <jerfosse@groupepublicis.net>
2018-09-24 18:37:47 +0200

    exercices sur les collections presques terminés

--- src/ii_collections/n17MaxMin.kt
+++ src/ii_collections/n17MaxMin.kt
@@ -7,10 +7,15 @@ fun example4() {
 
 fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
     // Return a customer whose order count is the highest among all customers
-    todoCollectionTask()
+    return this.customers
+            .sortedByDescending { it.orders.count() }
+            .first()
 }
 
 fun Customer.getMostExpensiveOrderedProduct(): Product? {
     // Return the most expensive product which has been ordered
-    todoCollectionTask()
+    return this.orders
+            .flatMap { it.products }
+            .sortedByDescending { it.price }
+            .first()
 }
--- src/ii_collections/n23CompoundTasks.kt
+++ src/ii_collections/n23CompoundTasks.kt
@@ -2,17 +2,26 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return this.customers
+            .filter { it.orderedProducts.contains(product) }
+            .toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    return this.orders
+            .filter { it.isDelivered }
+            .flatMap { it.products }
+            .sortedByDescending { it.price }
+            .first()
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    return this.customers
+            .flatMap { it.orders }
+            .fold(0) {acc, order -> acc + order.products.filter { it == product }.count() }
+
 }
